454792ce0a0709aceb1553dfbda05351bcd5ae71
Doug Gerber <dojoh@outlook.com>
2013-09-22 20:42:21 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Caccia::Application.config.secret_key_base = '908a50ba17fe9b4b7c5790f963d29ed39829a0e916ea1739f56054994b4786277cd89106b1a3de3137911f352a3a595eb0c5f641dfbeb1bbf3136ea9e3189e4e'
