1159f502ae455504ea2c3c419e3f1426119d4982
Doug Gerber <dojoh@outlook.com>
2013-10-25 11:26:01 -0400

    Pass Model Tests

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,2 +1,7 @@
 class Musician < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :genre
+  validates_presence_of :location
 end
--- spec/models/musician_spec.rb
+++ spec/models/musician_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
-describe User do
+describe Musician do
 
-	it { should have_valid(:first_name).when('John') }
-	it { should_not have_valid(:first_name).when(nil, '') }
-
-	it { should have_valid(:last_name).when('Smith') }
-	it { should_not have_valid(:last_name).when(nil, '') }
+	it { should have_valid(:name).when('John Smith') }
+	it { should_not have_valid(:name).when(nil, '') }
 
 	it { should have_valid(:email).when('user@example') }
 	it { should_not have_valid(:email).when(nil, '') }
