c23add4fe7089b9160601912d9cdee01f025887a
Doug Gerber <dojoh@outlook.com>
2013-10-25 11:32:31 -0400

    Edit Schema

--- db/migrate/20131025150149_create_musicians.rb
+++ db/migrate/20131025150149_create_musicians.rb
@@ -2,10 +2,10 @@ class CreateMusicians < ActiveRecord::Migration
   def change
     create_table :musicians do |t|
       t.integer :user_id
-      t.string :name
-      t.string :email
-      t.string :genre
-      t.string :location
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :genre, null: false
+      t.string :location, null: false
 
       t.timestamps
     end
